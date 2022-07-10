5667a99036b2abcecd9dde6169ddf6ee5c5e40b9
666WannaCry666 <usov3124@gmail.com>
2022-07-10 19:19:23 +0700

    Готовоgit add .!

--- js/index.js
+++ js/index.js
@@ -1,5 +1,45 @@
-// Массив для вывода в консоль id изображений 
+$(document).ready(function() {
+
+// Массив для вывода в консоль id изображений перенесенных в редактируемую зону
 let pictureList = [];
+
+function editngRestriction(selected) {
+  if (selected.prev().length == 0 && selected.next().length == 0) {
+    $('#move-up').css("display", "none");
+    $('#move-down').css("display", "none");
+    $('#delete').css("display", "inline");
+    return;
+  }
+  else if (selected.prev().length == 0) {
+    $('#move-down').css("display", "inline");
+    $('#move-up').css("display", "none");
+    $('#delete').css("display", "inline");
+    return;
+  }
+  else if (selected.next().length == 0) {
+    $('#move-down').css("display", "none");
+    $('#move-up').css("display", "inline");
+    $('#delete').css("display", "inline");
+    return;
+  }
+  $('#move-down').css("display", "inline");
+  $('#move-up').css("display", "inline");
+  $('#delete').css("display", "inline");
+  return;
+}
+
+function selectedClickEvent() {
+  //Добавление границы выбранному блоку
+  $('.dropped-img').removeClass("selected-block");
+  $(this).addClass("selected-block");
+  // Переключение в раздел Content
+  $('.templates').css("display", "none");
+  $('.edit').css("display", "flex");
+  $('#content').addClass("selected-partition");
+  $('#blocks').removeClass("selected-partition");
+  // Ограничение редактирования, если блок в самом верху или в самом низу
+  editngRestriction($('.selected-block'));
+}
 
 // jquery-ui drag'n'drop
 $('.template__img').draggable({
@@ -8,7 +48,7 @@ $('.template__img').draggable({
 
 $('.main-edit-area').droppable({
   activate: function (event, ui) {
-    $(this).append(ui.draggable.clone().css({left: 0, top: 0, opacity: 0.5, margin: "2.5px 0"}).addClass("drag-img").removeClass("template__img").removeAttr("id"));
+    $(this).append(ui.draggable.clone().addClass("drag-img").removeClass("template__img").removeAttr("id"));
     ui.draggable.css("opacity", 0.5);
   },
   deactivate: function(event, ui) {
@@ -16,10 +56,70 @@ $('.main-edit-area').droppable({
     ui.draggable.css("opacity", 1);
   },
   drop: function(event, ui) {
-    let drag = ui.draggable;
-    $(this).append(ui.draggable.clone().css({left: 0, top: 0, opacity: 1, cursor: 'default', margin: "2.5px 0"}).addClass("dropped-img").removeClass("template__img").removeAttr("id"));
-    pictureList.push(ui.draggable.attr("id"));
+    $('.dropped-img').removeClass("selected-block");
+    $(this).append(ui.draggable.clone().css("opacity", 1).addClass("dropped-img selected-block").removeClass("template__img").click(selectedClickEvent));
+    // if ($('.main-edit-area .dropped-img').length >= 5) {
+    //   $(this).css("overflow-y", "scroll");
+    // }
+    // else {
+    //   $(this).css("overflow-y", "auto");
+    // }
+    editngRestriction($('.selected-block'));
   },
   tolerance: "touch"
 });
-  
+
+// Переключение между разделами Blocks и Content
+$('#content').click(function() {
+  $('.templates').css("display", "none");
+  $('.edit').css("display", "flex");
+  $(this).addClass("selected-partition");
+  $('#blocks').removeClass("selected-partition");
+  editngRestriction($('.selected-block'));
+});
+
+$('#blocks').click(function() {
+  $('.edit').css("display", "none");
+  $('.templates').css("display", "flex");
+  $(this).addClass("selected-partition");
+  $('#content').removeClass("selected-partition");
+});
+
+// События для кнопок в разделе Content
+$('#move-up').click(function(e) {
+  e.preventDefault();
+  let movedBlock = $('.selected-block').clone().addClass("moved-block").removeClass("selected-block").click(selectedClickEvent);
+  movedBlock.insertBefore($('.selected-block').prev());
+  $('.selected-block').remove();
+  $('.moved-block').addClass("selected-block").removeClass("moved-block");
+  editngRestriction($('.selected-block'));
+})
+
+$('#move-down').click(function(e) {
+  e.preventDefault();
+  let movedBlock = $('.selected-block').clone().addClass("moved-block").removeClass("selected-block").click(selectedClickEvent);
+  movedBlock.insertAfter($('.selected-block').next());
+  $('.selected-block').remove();
+  $('.moved-block').addClass("selected-block").removeClass("moved-block");
+  editngRestriction($('.selected-block'));
+})
+
+$('#delete').click(function(e) {
+  e.preventDefault();
+  $('.selected-block').remove();
+  $(this).css("display", "none");
+  $('#move-up').css("display", "none");
+  $('#move-down').css("display", "none");
+})
+
+// Вывод в консоль массива с картинками
+$('#test').click(function(e) {
+  e.preventDefault();
+  $('.dropped-img').each(function(elem) {
+    pictureList.push($(this).attr("id"));
+  })
+  console.log(pictureList);
+  pictureList = [];
+})
+
+});
